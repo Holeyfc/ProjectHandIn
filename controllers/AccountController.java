4460ba4a2b1b837b22833f622aae88e60d2d5566
NHOBERG001 <nhoberg001@vplincsci068.ad.csbsju.edu>
2019-03-12 17:05:07 -0500

    First Entry, Abdi and Noah

---
+++ controllers/AccountController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package controllers;
+
+/**
+ * @author nhoberg001
+ *
+ */
+public class AccountController {
+
+	/**
+	 * 
+	 */
+	public AccountController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
