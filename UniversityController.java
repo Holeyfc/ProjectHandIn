27282c1bb21ac3942021fc97d6bb0402354b9503
Holeyfc <48495959+Holeyfc@users.noreply.github.com>
2019-03-12 17:17:15 -0500

    Add files via upload
    "First Entry, controllers and Admin"

---
+++ UniversityController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package controllers;
+
+/**
+ * @author nhoberg001
+ *
+ */
+public class UniversityController {
+
+	/**
+	 * 
+	 */
+	public UniversityController() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
