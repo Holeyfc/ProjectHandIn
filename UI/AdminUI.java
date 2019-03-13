cc5a578074ac31356ee23c9ef1202404ee7632de
TTORBERT001 <ttorbert001@vplincsci062.ad.csbsju.edu>
2019-03-13 16:24:13 -0500

    Organize UI

---
+++ UI/AdminUI.java
@@ -0,0 +1,42 @@
+/**
+ * Admin confirms that the account exist
+ */
+package UI;
+
+/**
+ * @author HoleyFanClub
+ * @version 2.0
+ *
+ */
+public class AdminUI extends Admin {
+
+ 
+ public AdminUI() {
+  // TODO Auto-generated constructor stub
+ }
+
+ /**
+  * Confirms that the user is correct and the password is correct, the account exist
+  */
+ public string confirmMessage()
+ {
+   
+ System.out.println("Successful Login!")
+ }
+ 
+ 
+ /**
+  * Confirms that there is no account or the user or password is incorrect
+  */
+ public string failMessage()
+ {
+   
+  System.out.println("Try again"); 
+  
+ }
+
+ public void viewListOfUser()
+ {
+   // TODO Auto-generated constructor stub
+ }
+}
