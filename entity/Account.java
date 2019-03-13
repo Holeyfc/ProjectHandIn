0695409d812dd8f3b43d5cbb0c504f48a3f3a3b8
TTORBERT001 <ttorbert001@vplincsci062.ad.csbsju.edu>
2019-03-13 14:44:43 -0500

    Account and User

--- entity/Account.java
+++ entity/Account.java
@@ -1,28 +1,22 @@
 //@author HoleyFanClub
-//@version 1.0
+//@version 2.0
 package entity;
-import javax.swing.*;
 import dblibrary.project.csci230.*;
-public class Account 
-{
- private String password;
- public String username;
- private char type;
- private char status;
- public static void main(String[] args) 
- {
-  this.username = JOptionPane.showInputDialog("please enter your username");
-  this.password = JOptionPane.showInputDialog("please enter your password");
-  login(username, password);
+public class Account {
+	
+	String password;
+	String username;
+	char status;
+	char type;
+	
+	public void logout()
+	{
+		
+	}
+	
+	public void login()
+	{
+	 UniversityDBLibrary lib = new UniversityDBLibrary(username, password)
+	}
   
- }
- public void logout()
- {
- }
- 
- public static void login(String username, String password)
- {
-  UniversityDBLibrary lib = new UniversityDBLibrary(username, password);
- }
-
 }
